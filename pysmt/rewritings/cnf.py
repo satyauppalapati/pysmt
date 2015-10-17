1f21c8f844a9e6f78f671ecedccd4d9c15ba346b
Marco Gario <marco.gario@gmail.com>
2015-10-17 19:30:43 +0200

    Fixed incorrect imports

--- pysmt/rewritings/cnf.py
+++ pysmt/rewritings/cnf.py
@@ -15,6 +15,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
+import pysmt.typing as types
 from pysmt.walkers import DagWalker
 
 
